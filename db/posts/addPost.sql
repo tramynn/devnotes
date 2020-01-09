e05de9e7cfd63094492e6d5078910a4b07a982e0
Tramy Nguyen <tramynn2@gmail.com>
2020-01-09 13:54:26 -0600

    add post works backend

--- db/posts/addPost.sql
+++ db/posts/addPost.sql
@@ -1,2 +1,2 @@
 INSERT INTO posts (category_name, title, content, date_published, user_id)
-VALUES ($1, $2, $3, CURRENT_TIMESTAMP, $4)
+VALUES ($1, $2, $3, CURRENT_TIMESTAMP, $4);
--- server/controllers/postsController.js
+++ server/controllers/postsController.js
@@ -6,7 +6,14 @@ async function allPosts(req, res) {
 }
 
 async function addPost(req, res) {
+  const { category_name, title, content } = req.body;
+  const { user_id } = req.session.user.user_id;
 
+  const db = req.app.get("db");
+
+  const addedPost = await db.posts.addPost([category_name, title, content, user_id]);
+
+  res.status(200).json(addedPost);
 }
 
 async function editPost(req, res) {
