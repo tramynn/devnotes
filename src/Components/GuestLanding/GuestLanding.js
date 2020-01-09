e2a1951b36c46be8cf68dc106534f96b5447a203
Tramy Nguyen <tramynn2@gmail.com>
2020-01-09 14:57:02 -0600

    created guestlaanding and home

---
+++ src/Components/GuestLanding/GuestLanding.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class GuestLanding extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+      <h1>Guest Landing</h1>
+      </div>
+    )
+  }
+}
+
+export default GuestLanding;
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Home</h1>
+      </div>
+    )
+  }
+}
+
+export default Home;
