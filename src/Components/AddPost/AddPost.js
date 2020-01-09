66de31749de750236d8a2f2a90295ad83654375c
Tramy Nguyen <tramynn2@gmail.com>
2020-01-09 14:59:45 -0600

    set up add post and user profile

---
+++ src/Components/AddPost/AddPost.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class AddPost extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Add a DevNote</h1>
+      </div>
+    )
+  }
+}
+
+export default AddPost;
---
+++ src/Components/UserProfile/UserProfile.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class UserProfile extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>My Profile</h1>
+      </div>
+    )
+  }
+}
+
+export default UserProfile;
