65a72664ecdcb7451ad0fc44440c0d5e1e7addb4
Tramy Nguyen <tramynn2@gmail.com>
2020-01-09 15:21:04 -0600

    set up store

---
+++ src/reducks/store.js
@@ -0,0 +1,11 @@
+import { createStore, combineReducers, applyMiddleware} from 'redux';
+import promise from "redux-promise-middleware";
+import authReducer from "./reducers/authReducer";
+import postsReducer from "./reducers/postsReducer";
+
+const rootReducer = combineReducers({
+  authReducer,
+  postsReducer
+})
+
+export default createStore(rootReducer, applyMiddleware(promise))
